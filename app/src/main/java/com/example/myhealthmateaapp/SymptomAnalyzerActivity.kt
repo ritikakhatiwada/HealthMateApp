c1adf6b776dc0591164718faf1d4fe95ebc22b7e
ritikakhatiwada <ritikakhatiwada67@gmail.com>
2026-01-26 18:46:12 +0545

    Symptom Analyzer Added

--- app/src/main/java/com/example/myhealthmateaapp/SymptomAnalyzerActivity.kt
+++ app/src/main/java/com/example/myhealthmateaapp/SymptomAnalyzerActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -96,5 +97,12 @@ fun SymptomAnalyzerScreen(
                 }
             }
         }
+    }
+}
+@Preview
+@Composable
+fun SymptomAnalyzer(){
+    SymptomAnalyzerScreen{
+
     }
 }
