b12e90abcc16c22d2c4a398b9133384c7457e0ef
ritikakhatiwada <ritikakhatiwada67@gmail.com>
2026-01-30 21:45:54 +0545

    Insturmentedtesting

--- app/src/androidTest/java/com/example/myhealthmateaapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/myhealthmateaapp/ExampleInstrumentedTest.kt
@@ -1,24 +1,48 @@
 package com.example.myhealthmateaapp
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.core.app.ActivityScenario.launch
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class LoginInstrumentedTest {
+
+    @Before
+    fun setup() {
+        Intents.init()
+        launch(LoginActivity::class.java)
+    }
+
+    @After
+    fun tearDown() {
+        Intents.release()
+    }
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.myhealthmateaapp", appContext.packageName)
+    fun login_withValidCredentials_opensMainActivity() {
+
+        // Type email
+        onView(withId(R.id.etEmail))
+            .perform(typeText("test@gmail.com"), closeSoftKeyboard())
+
+        // Type password
+        onView(withId(R.id.etPassword))
+            .perform(typeText("123456"), closeSoftKeyboard())
+
+        // Click login
+        onView(withId(R.id.btnLogin))
+            .perform(click())
+
+        // Verify MainActivity is opened
+        Intents.intended(hasComponent(MainActivity::class.java.name))
     }
 }
